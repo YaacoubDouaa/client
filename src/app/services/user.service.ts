e1fa9e4cac7f2670df0da828e7d27ba6c607f4f3
douaayacoub <132510423+douaayacoub@users.noreply.github.com>
2026-05-07 13:19:55 +0100

    commit

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -13,7 +13,7 @@ export interface User {
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl = 'http://prod-alb-1166213154.us-east-1.elb.amazonaws.com/api/users';
+  private apiUrl = 'http://backend-alb-1552392847.us-east-1.elb.amazonaws.com/api/users';
 
   constructor(private http: HttpClient) { }
 
